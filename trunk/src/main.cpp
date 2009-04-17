96550fe2cbdcc933336bd023bbe5d39380bece5e
kongyang80 <kongyang80@643bfc8c-1e2e-11de-b69a-45dc1c5e4070>
2009-04-17 15:13:33 +0000

    Added a windows app icon

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -2,12 +2,21 @@
 #include <FL/Fl.H>
 #include "ui.h"
 
+#ifdef WIN32
+#include <FL/x.H>
+#include "resource.h"
+#endif
+
 int
 main(int argc, char **argv) {
 
     UserInterface *ui = new UserInterface();
     Fl::scheme("plastic");
     Fl::visual(FL_DOUBLE|FL_INDEX);
+
+#ifdef WIN32
+    ui->mainWindow->icon((char *)LoadIcon(fl_display, MAKEINTRESOURCE(IDI_ICON)));
+#endif
 
     glutInit(&argc, argv);
     ui->show(argc, argv);
