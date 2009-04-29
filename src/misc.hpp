07dbb55865291a747e70291eefa1c1a255082a78
mikolalysenko <mikolalysenko@643bfc8c-1e2e-11de-b69a-45dc1c5e4070>
2009-04-29 00:11:09 +0000

    Forgot to check in file!  Whoops

---
+++ src/misc.hpp
@@ -0,0 +1,32 @@
+//Has aliases for aligned types
+
+#ifndef MISC_H
+#define MISC_H
+
+#include <Eigen/Core>
+
+#include <vector>
+#include <map>
+#include <list>
+#include <deque>
+#include <queue>
+
+//Aligned types
+//Eigen decided to change the way stdvector worked, so you now have to type Eigen::aligned_allocator<T> for each thing *groan*
+//This hack saves some keystrokes, for example to get a vector you type in:
+//
+//	aligned<Vector3d>::vector x
+//
+// Which is kind of annoying, but what are you gonna do? This is the best I could come up with within the limitations of C++
+//
+template<typename T, typename V=int> struct aligned
+{
+	typedef std::vector<T, Eigen::aligned_allocator<T> > vector;
+	typedef std::list<T, Eigen::aligned_allocator<T> > list;
+	typedef std::deque<T, Eigen::aligned_allocator<T> > deque;
+	typedef std::priority_queue<T, vector> priority_queue;
+	typedef std::map< T, V, std::less<int>, Eigen::aligned_allocator<V> > map;
+};
+
+#endif
+
