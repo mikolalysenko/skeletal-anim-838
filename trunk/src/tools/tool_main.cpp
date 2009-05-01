438ce0b459e7391251da56184e5fcd402761048a
mikolalysenko <mikolalysenko@643bfc8c-1e2e-11de-b69a-45dc1c5e4070>
2009-05-01 17:50:44 +0000

    Added some more stuff to the motool skeleton

--- trunk/src/tools/tool_main.cpp
+++ trunk/src/tools/tool_main.cpp
@@ -4,7 +4,9 @@
 #include <Eigen/StdVector>
 
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <cstdlib>
 
 #include <misc.hpp>
 #include <skeleton.hpp>
@@ -14,17 +16,35 @@ using namespace std;
 using namespace Eigen;
 using namespace Skeletal;
 
+//Creates a motion graph from a motion file
+void create_graph(const string& mo_file)
+{
+}
 
+//Adds a motion to a motion graph
+void add_motion(const string& mo_file, const string& graph_file)
+{
+}
+
+
+//Synthesizes a new motion
+void synthesize_motion(int l, const string& graph_file)
+{
+}
+
+
+//Prints out a helpful message
 void print_help()
 {
 	cout << endl
 		 << "motool : Motion Graph Edit Tool" << endl
 		 << "-------------------------------------------------" << endl
 		 << "This program processes BVH format character motions and motion graphs." << endl
+		 << "All results are written to stdout because I am a lazy programmer." << endl
 		 << endl
 		 << "Use Cases:" << endl
 		 << "  Create a motion graph:" << endl
-		 << "     motool -c <base_motion.bvh> <graph.mog>" << endl
+		 << "     motool -c <base_motion.bvh>" << endl
 		 << endl
 		 << "  Add a motion to a graph:" << endl
 		 << "     motool -a <motion.bvh> <graph.mog>" << endl
@@ -35,9 +55,43 @@ void print_help()
 		 << endl;
 }
 
+//Program start point
 int main(int argc, char** argv)
 {
-	print_help();
+	if(argc < 2)
+	{
+		cout << "Incorrect arguments" << endl;
+		print_help();
+		exit(1);
+	}
+	
+	string command = argv[1];
+	
+	if(command == "-c")
+	{
+		assert(argc == 3);
+		string motion_file(argv[2]);
+		create_graph(motion_file);
+	}
+	else if(command == "-a")
+	{
+		assert(argc == 4);
+		string motion_file(argv[2]), graph_file(argv[3]);
+		add_motion(motion_file, graph_file);
+	}
+	else if(command == "-s")
+	{
+		assert(argc == 4);
+		int l = atoi(argv[2]);
+		string graph_file(argv[3]);
+		synthesize_motion(l, graph_file);
+	}
+	else
+	{
+		cout << "Unknown command: " << command << endl;
+		print_help();
+		exit(1);
+	}
 
 	return 0;
 }
