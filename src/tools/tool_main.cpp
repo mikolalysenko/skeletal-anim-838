ccd097848d4de7c0fa5ea61f43a41d51b254d0ac
mikolalysenko <mikolalysenko@643bfc8c-1e2e-11de-b69a-45dc1c5e4070>
2009-05-01 17:42:22 +0000

    Put in a sketch of how the motion tool should work

--- src/tools/tool_main.cpp
+++ src/tools/tool_main.cpp
@@ -1,9 +1,44 @@
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <Eigen/LU>
+#include <Eigen/StdVector>
+
 #include <iostream>
+#include <vector>
+
+#include <misc.hpp>
+#include <skeleton.hpp>
+#include <mograph.hpp>
 
 using namespace std;
+using namespace Eigen;
+using namespace Skeletal;
+
+
+void print_help()
+{
+	cout << endl
+		 << "motool : Motion Graph Edit Tool" << endl
+		 << "-------------------------------------------------" << endl
+		 << "This program processes BVH format character motions and motion graphs." << endl
+		 << endl
+		 << "Use Cases:" << endl
+		 << "  Create a motion graph:" << endl
+		 << "     motool -c <base_motion.bvh> <graph.mog>" << endl
+		 << endl
+		 << "  Add a motion to a graph:" << endl
+		 << "     motool -a <motion.bvh> <graph.mog>" << endl
+		 << endl
+		 << "  Synthesize a random motion:" << endl
+		 << "     motool -s <length> <graph.mog>" << endl
+		 << endl
+		 << endl;
+}
 
 int main(int argc, char** argv)
 {
-	cout << "test" << endl;
+	print_help();
+
 	return 0;
 }
+
